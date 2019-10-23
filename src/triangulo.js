4b02232666f4b3b6f276f9ba67fcc3e0f593adc8
Izabella Melo <cmeloizabella@gmail.com>
2019-10-23 18:28:42 -0300

    implement valido function

--- src/triangulo.js
+++ src/triangulo.js
@@ -4,10 +4,28 @@ function Triangulo(a,b,c) {
     this.c = c;
   }
 
+
+// Para construir um triângulo é necessário que a 
+// medida de qualquer um dos lados seja menor que a 
+// soma das medidas dos outros dois e maior que o valor 
+// absoluto da diferença entre essas medidas.
+Triangulo.prototype.valido = function () {
+	if(this.a <= 0 || this.b <= 0 || this.c <= 0) {
+		return false;
+	} 
+
+	if( ((this.a + this.b > this.c) && (Math.abs(this.a - this.b) < this.c)) ||
+		((this.b + this.c > this.a) && (Math.abs(this.b - this.c) < this.a)) || 
+		((this.c + this.a > this.b) && (Math.abs(this.c - this.a) < this.b)) ) {
+		return true;
+	}
+
+	return false;
+}
   
-  Triangulo.prototype.toString = function () {
-    return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
-  };
-  
-  
-  module.exports = Triangulo;
+Triangulo.prototype.toString = function () {
+return `Lado A ${this.a}, Lado B ${this.b}, Lado C ${this.c}`
+};
+
+
+module.exports = Triangulo;
