d0e92034f62d01929330856b7fd4be9299af7627
Izabella Melo <cmeloizabella@gmail.com>
2019-10-24 13:39:34 -0300

    implement all functions and tests

--- src/api.js
+++ src/api.js
@@ -2,12 +2,103 @@ const express = require('express');
 const app = express();
 
 app.get('/converterTemperatura', (req,res) => {
-	valor = parseInt(req.query.valor)
-	
-	res.json({
-		resposta: 30,
-		status:'ok'
-	});
+	var val = parseInt(req.query.valor)
+
+	if(req.query.de === "C") {
+		if(req.query.para === "F") {
+			
+			var ans = (val*(9/5)) + 32;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'ok'
+			});
+		} else if(req.query.para == "K") {
+			
+			var ans = val + 273.15;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else if(req.query.para == "C") {
+			
+			var ans = val;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else {
+			res.json({
+				resposta: -1,
+				status:'KO'
+			});
+		}
+	} else if(req.query.de === "F") {
+		if(req.query.para === "C") {
+			
+			var ans = (val - 32)*(5/9);
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'ok'
+			});
+		} else if(req.query.para == "K") {
+			
+			var ans = (val - 32)*(5/9) + 273.15;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else if(req.query.para == "F") {
+			
+			var ans = val;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else {
+			res.json({
+				resposta: -1,
+				status:'KO'
+			});
+		}
+	} else if(req.query.de === "K") {
+		if(req.query.para === "C") {
+			
+			var ans = val - 273.15;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'ok'
+			});
+		} else if(req.query.para == "F") {
+			
+			var ans = (val - 273.15)*(9/5) + 32;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else if(req.query.para == "K") {
+			
+			var ans = val;
+
+			res.json({
+				resposta: Number(ans.toFixed(2)),
+				status:'OK'
+			});
+		} else {
+			res.json({
+				resposta: -1,
+				status:'KO'
+			});
+		}
+	}
+
 });
 
 app.listen(5000, (err)=>{
--- src/test/testes-api.js
+++ src/test/testes-api.js
@@ -7,60 +7,167 @@ chai.use(chaiHttp);
 
 // FROM CELSIUS
 describe('Testando conversão de C para F', () => {
-	var tempFrom = 20;
-	var tempTo = 68;
+	var tempFrom = 20.00;
+	var tempTo = 68.00;
 	var from = "C"
 	var to = "F";
 
-	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, () => {
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
 		chai.request(api)
 			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
 			.end((err, res) => {
 
-			res.should.have.status(200);
-			res.body.should.have.property('resposta').eql(tempTo);
-			done();
-		});
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
 	});
 });
 
 describe('Testando conversão de C para K', () => {
-	var tempFrom = 20;
-	var tempTo = 293,15;
+	var tempFrom = 20.00;
+	var tempTo = 293.15;
 	var from = "C"
 	var to = "K";
 
-	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, () => {
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
 		chai.request(api)
 			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
 			.end((err, res) => {
 
-			res.should.have.status(200);
-			res.body.should.have.property('resposta').eql(tempTo);
-			done();
-		});
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
 	});
 });
 
 describe('Testando conversão de C para C', () => {
-	var tempFrom = 25;
-	var tempTo = 20;
+	var tempFrom = 20.00;
+	var tempTo = 20.00;
 	var from = "C"
 	var to = "C";
 
-	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, () => {
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
 		chai.request(api)
 			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
 			.end((err, res) => {
 
-			res.should.have.status(200);
-			res.body.should.have.property('resposta').eql(tempTo);
-			done();
-		});
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
 	});
 });
 
-//
+// FROM FAHRENHEIT
+describe('Testando conversão de F para C', () => {
+	var tempFrom = 32.00;
+	var tempTo = 0.00;
+	var from = "F"
+	var to = "C";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
+
+describe('Testando conversão de F para K', () => {
+	var tempFrom = 50.00;
+	var tempTo = 283.15;
+	var from = "F"
+	var to = "K";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
 
 
+describe('Testando conversão de F para F', () => {
+	var tempFrom = 283.00;
+	var tempTo = 283.00;
+	var from = "F"
+	var to = "F";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
+
+// FROM KELVIN
+describe('Testando conversão de K para C', () => {
+	var tempFrom = 0.00;
+	var tempTo = -273.15;
+	var from = "K"
+	var to = "C";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
+
+describe('Testando conversão de K para F', () => {
+	var tempFrom = 10.00;
+	var tempTo = -441.67;
+	var from = "K"
+	var to = "F";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
+
+describe('Testando conversão de K para K', () => {
+	var tempFrom = 10.00;
+	var tempTo = 10.00;
+	var from = "K"
+	var to = "K";
+
+	it('Teste deve retornar a temperatura em ' + from + ' equivalente a temperatura passada em ' + to, (done) => {
+		chai.request(api)
+			.get('/converterTemperatura?valor='+ tempFrom +'&de=' + from + '&para=' + to)
+			.end((err, res) => {
+
+				res.should.have.status(200);
+				res.body.should.have.property('resposta').eql(tempTo);
+				done();
+			});
+	});
+});
 
